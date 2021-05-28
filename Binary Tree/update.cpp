4bb456f907c52425bc5290318dcc67ef4bbe0ab4
harshit987 <hakumar@iitk.ac.in>
2021-05-29 03:50:58 +0530

    Add AVL insert, left rotate, right rotate, delete

--- Binary Tree/update.cpp
+++ Binary Tree/update.cpp
@@ -1,16 +1,16 @@
 #include<iostream>
 #include "struct.h"
+#include "update.h"
+#include "query.h"
 
-
-Node* insert(Node* root,int val)
+Node* insert(Node* root,Node* z)
 {
 	Node* parent = NULL;
 	Node* x = root;
 
-	Node* z = createNode(val);
-
 	while(x!=NULL)
 	{
+		x->height++;
 		parent = x;
 		if(x->val<z->val)
 			x = x->right;
@@ -26,6 +26,46 @@ Node* insert(Node* root,int val)
 			parent->right = z;
 		else
 			parent->left = z;
+	}
+	return root;
+}
+
+Node* del(Node* root,Node* z)
+{
+	Node* parent = z->parent;
+	if(z->left==NULL&&z->right==NULL)
+	{
+		if(parent==NULL)
+			root = NULL;
+		else if(parent->right==z)
+			parent->right=NULL;
+		else
+			parent->left= NULL;
+	}
+	else if(z->left==NULL)
+	{
+		if(parent==NULL)
+			root = z->right;
+		else if(parent->left == z) 
+			parent->left = z->right;
+		else
+			parent->right = z->right;
+	}
+	else if(z->right == NULL)
+	{
+		if(parent==NULL)
+			root = z->left;
+		else if(parent->left == z)
+			parent->left = z->left;
+		else
+			parent->right = z->left;
+	}
+	else
+	{
+		Node* y = Minimum(root,z->right);
+		z->val = y->val;
+		Node* p = y->parent;
+		p->left = y->right; 
 	}
 	return root;
 }
